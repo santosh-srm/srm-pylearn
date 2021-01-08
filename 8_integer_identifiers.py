48d12666e8cca93233d0a1b12a69f14cbb7dea5f
santosh <santoshrao.m@gmail.com>
2021-01-08 12:01:37 +0530

    8th program

---
+++ 8_integer_identifiers.py
@@ -0,0 +1,20 @@
+
+a=15
+b=40
+c=a+b
+
+print(a, b, c) # 10 40 50
+print (a+b) # 50
+
+print("a = ", a)
+print("b = ", b)
+print("c = ", c)
+
+"""
+Output:
+15 40 50
+50
+a =  15
+b =  40
+c =  55
+"""
