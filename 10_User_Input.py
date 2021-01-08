42ada449391c8e519325ba4ba1fb4938cf293cfa
santosh <santoshrao.m@gmail.com>
2021-01-08 12:58:29 +0530

    New Program

---
+++ 10_User_Input.py
@@ -0,0 +1,15 @@
+"""10_user_input.py"""
+
+name = input("Enter Name: ")
+time = input("The Time of the day is: ")
+
+print(f'Hello {name}, Great {time}!!')
+
+"""
+Output
+
+Enter Name: Samarth
+The Time of the day is: Afternoon!
+Hello Samarth, Great Afternoon!!!
+
+"""
